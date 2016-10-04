53fc3f7142afc58645e51760af56e273002fa80f
minkug <mmskmk@ajou.ac.kr>
2016-10-04 11:42:36 +0900

    test

--- lab4_calc2/src/lab4_Calculator/Calculator.java
+++ lab4_calc2/src/lab4_Calculator/Calculator.java
@@ -4,13 +4,12 @@ public class Calculator {
 
 
 	public double add(double x, double y){
-		return 0;
+		return x+y;
 	} 
 	
 	
 	public double subtract(double x, double y){
 		double result;
-		//hongki
 		
 		result = x-y;
 		return result;
@@ -20,7 +19,7 @@ public class Calculator {
 	public double multiply(double x, double y){
 		
 		
-		return 0;
+		return x*y;
 	}
 	
 	public double divide(double x, double y){
@@ -55,11 +54,11 @@ public class Calculator {
 		double r4 = cal.multiply(num1, num2);
 		double r5 = cal.remainder(num1, num2);
 
-		System.out.printf("%f + %f = %f", num1, num2, r1);
-		System.out.printf("%f - %f = %f", num1, num2, r2);
-		System.out.printf("%f / %f = %f", num1, num2, r3);
-		System.out.printf("%f *  %f = %f", num1, num2, r4);
-		System.out.printf("%f %  %f = %f", num1, num2, r5);
+		System.out.println("Add : "+ r1);
+		System.out.println("Subtract : "+r2);
+		System.out.println("Divide : "+r3);
+		System.out.println("Multiply : "+r4);
+		System.out.println("Remainder : "+r5);
 
 	}
 
