b6a8d761b0cd979f619190444483ec1be9c4105d
ajou <ajou@ajou-PC>
2016-10-04 11:40:09 +0900

    aa

--- lab4_calc2/src/lab4_Calculator/Calculator.java
+++ lab4_calc2/src/lab4_Calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
 	} 
 	public String toString(){
 		String allMembers="{Leader:SangJinAhn(Sangjin9988), Programmer1:YooJinBae(youjinbae), Programmer2:SungWookHong(brood1346), http://github.com/SangjinAhn/oop-lab4 }";
-		//f
+		//bae1
 		return allMembers;
 	}
 	
