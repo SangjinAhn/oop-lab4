6b6015776c9bd62501c5a993fd2ed8321953caa4
SangjinAhn <sangjin9988@naver.com>
2016-10-04 10:50:27 +0900

    hello

---
+++ lab4_calc2/src/lab4_Calculator/Calculator.java
@@ -0,0 +1,45 @@
+package lab4_Calculator;
+
+public class Calculator {
+
+	
+	
+	
+	public double add(double x, double y){
+		
+		return 0;
+	} 
+	
+	
+	public double subtract(double x, double y){
+		
+		return 0;
+	}
+	
+	public double multiply(double x, double y){
+		
+		return 0;
+	}
+	
+	public double divide(double x, double y){
+		
+		return 0;
+	}
+	
+	public double remainder(double x, double y){
+		
+		return 0;
+	}
+	
+
+	
+	public static void main(String args[])
+	
+	{
+		
+		
+		
+		
+	}
+	
+}
