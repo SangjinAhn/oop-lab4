4c01290b1cbdb3fd2f48ceda3ed6d8ae4f7b706f
minkug <mmskmk@ajou.ac.kr>
2016-10-04 11:51:34 +0900

    최종본(Final)

--- lab4_calc2/src/lab4_Calculator/Calculator.java
+++ lab4_calc2/src/lab4_Calculator/Calculator.java
@@ -2,51 +2,46 @@ package lab4_Calculator;
 
 public class Calculator {
 
-
-	public double add(double x, double y){
-		double result=x+y;
+	public double add(double x, double y) {
+		double result = x + y;
 		return result;
 
-	} 
-	public String toString(){
-		String allMembers="{Leader:SangJinAhn(Sangjin9988), Programmer1:YooJinBae(youjinbae), Programmer2:SungWookHong(brood1346), http://github.com/SangjinAhn/oop-lab4 }";
-		//bae2
+	}
+
+	public String toString() {
+		String allMembers = "{Leader:SangJinAhn(Sangjin9988), Programmer1:YooJinBae(youjinbae), Programmer2:SungWookHong(brood1346), http://github.com/SangjinAhn/oop-lab4 }";
+		// bae2
 		return allMembers;
 	}
-	
-	public double subtract(double x, double y){
+
+	public double subtract(double x, double y) {
 		double result;
-		
-		result = x-y;
+
+		result = x - y;
 		return result;
-		
+
 	}
-	
-	public double multiply(double x, double y){
-		
-		
-		return x*y;
+
+	public double multiply(double x, double y) {
+
+		return x * y;
 	}
-	
-	public double divide(double x, double y){
+
+	public double divide(double x, double y) {
 		double result;
-		result = x/y;
-		if(x==0)
+		result = x / y;
+		if (x == 0)
 			System.out.println("You can divide by zero");
 		return result;
 	}
-	
-	public double remainder(double x, double y){
+
+	public double remainder(double x, double y) {
 		double result;
-		result = x%y;
-		
+		result = x % y;
+
 		return result;
 	}
-	
 
-	
-	
-	
 	public static void main(String args[])
 
 	{
@@ -60,12 +55,14 @@ public class Calculator {
 		double r4 = cal.multiply(num1, num2);
 		double r5 = cal.remainder(num1, num2);
 
-		System.out.println("Add : "+ r1);
-		System.out.println("Subtract : "+r2);
-		System.out.println("Divide : "+r3);
-		System.out.println("Multiply : "+r4);
-		System.out.println("Remainder : "+r5);
+		String member = cal.toString();
 
+		System.out.println("Add : " + r1);
+		System.out.println("Subtract : " + r2);
+		System.out.println("Divide : " + r3);
+		System.out.println("Multiply : " + r4);
+		System.out.println("Remainder : " + r5);
+		System.out.println(member);
 	}
 
 }
